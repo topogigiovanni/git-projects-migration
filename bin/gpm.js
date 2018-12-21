8a918a24f50a892a871f3af1e43d4fbc6aa85af5
Giovanni Mansueto <giovanni.mansueto@dcg.com.br>
2018-12-21 17:14:59 -0200

    created gpm plugin with import and export actions

---
+++ bin/gpm.js
@@ -0,0 +1,34 @@
+const commandLineArgs = require('command-line-args');
+const gpm = require('../lib/index.js');
+
+/* first - parse the main command */
+const mainDefinitions = [{
+	name: 'command',
+	defaultOption: true
+}];
+
+const mainOptions = commandLineArgs(mainDefinitions, {
+	stopAtFirstUnknown: true
+});
+const argv = mainOptions._unknown || [];
+
+console.log('mainOptions\n===========');
+console.log(mainOptions);
+
+/*
+	second - parse the merge command options
+	ref: https://github.com/75lb/command-line-args/wiki/Implement-command-parsing-(git-style)
+*/
+if (mainOptions.command === 'export') {
+	gpm.exportProjects();
+
+	return;
+}
+
+if (mainOptions.command === 'import') {
+	gpm.importProjects();
+
+	return;
+}
+
+console.log('Command not found, try use "import" or "export"');
--- bin/test-module.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-var lib = require('../lib/index.js');
-var greeting = lib.sayHello('Bret');
-
-console.log(greeting);
